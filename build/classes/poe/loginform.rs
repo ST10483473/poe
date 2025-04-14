5886b52e0775366c580fa66f47da8b983c953954
muswiki <ST10483473@rcconnect.edu.za>
2025-04-14 12:23:26 +0000

    Initial commit

---
+++ build/classes/poe/loginform.rs
@@ -0,0 +1,8 @@
+loginform
+loginform$7
+loginform$3
+loginform$4
+loginform$5
+loginform$6
+loginform$1
+loginform$2
---
+++ src/poe/RegisterandLOGIN.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poe;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class RegisterandLOGIN {
+   public static String Usernamem="";
+   public static String Passwordd="";
+    
+}
